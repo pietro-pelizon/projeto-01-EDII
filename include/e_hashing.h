26018254a90ca4e6ee37f26e49d65361dd90688a
ppelizon <pietrofernandopelizon@gmail.com>
2026-04-03 00:53:10 -0300

    feat: Adição de uma função que retorna o tamanho do hashmap
    Mudança na forma que trato os dados contido na estrutura. A estrutura lidará somente com o mesmo tipo ao longo de todo seu tempo de execução. Isso deixa as coisas mais diretas e facilita a implementação

--- include/e_hashing.h
+++ include/e_hashing.h
@@ -2,7 +2,6 @@
 #define E_HASHING
 
 #include <inttypes.h>
-#include <stddef.h>
 #include <stdbool.h>
 
 typedef struct StEHashing eHashing;
@@ -11,32 +10,32 @@ typedef struct StBucket bucket;
 /// @brief Inicializa o Hash Map
 /// @param filename Nome do arquivo que será utilizado pelo hash extensível
 /// @param bucket_size Tamanho total do bucket (em bytes)
-/// @param destructor Ponteiro para a função que tratará a destruição do tipo de dado inserido no hash map
 /// @return Retorna um ponteiro para o hash map já criado
-eHashing *e_hashing_init(const char *filename, size_t bucket_size, void (*destructor)(void *));
+eHashing *e_hashing_init(const char *filename, uint32_t record_size, uint32_t bucket_size);
 
-/// @brief Insere um dado no Hash Map, pra isso, precisamos ter sua chave da função de hashing
+uint64_t e_hashing_capacity(eHashing *map);
+
+/// @brief Insere um dado no Hash Map, para isso, precisamos ter sua chave da função de hashing
 /// e o seu tamanho (em bytes)
 /// @param map Ponteiro para o Hash Map
 /// @param data O dado que será inserido - usamos "Void *" para podermos inserir qualquer tipo de dado
 /// @param key Chave criada pela função de hashing para o dado a ser inserido
-/// @param data_size Tamanho do dado a ser inserido (em bytes)
 /// @return Retorna true caso a inserção ocorra com sucesso e false caso o contrário
-bool e_hashing_insert(eHashing *map, void *data, uint64_t key, size_t data_size);
+bool e_hashing_insert(eHashing *map, void *data, uint64_t key);
 
 /// @brief Busca um dado no Hash Map
 /// @param map Ponteiro para o Hash Map
 /// @param key Chave do dado a ser buscado
-/// @param data_size PPonteiro de SAÍDA que receberá o tamanho do dado encontrado (em bytes)
+/// @param out_data Dado que será escrito diretamente no buffer do usuário, aí não teremos a necessidade
+/// de se preocupar com a memória alocada
 /// @return Retorna um ponteiro para o dado, ou NULL não caso seja encontrado
-void *e_hashing_search(eHashing *map, uint64_t key, size_t *data_size);
+bool e_hashing_search(eHashing *map, uint64_t key, void *out_data);
 
 /// @brief Remove um dado do Hash Map e o retorna para o usuário
 /// @param map Ponteiro para o Hash Map
 /// @param key Chave do dado a ser removido
-/// @param out_data_size Ponteiro de SAÍDA que receberá o tamanho do dado removido (em bytes)
 /// @return Retorna o dado que foi removido. O usuário é responsável por liberar (free) essa memória
-void *e_hashing_remove(eHashing *map, uint64_t key, size_t *out_data_size);
+void *e_hashing_remove(eHashing *map, uint64_t key);
 
 /// @brief Limpa toda a estrutura do Hash Map
 /// @param map Ponteiro para o Hash Map
