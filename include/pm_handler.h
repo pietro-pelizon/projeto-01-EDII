ed21a98448b5c9606880cad9aef01bef3437ca65
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-04-13 14:03:51 -0300

    feat: Handler do arquivo .pm

---
+++ include/pm_handler.h
@@ -0,0 +1,8 @@
+#ifndef PROJETO_01_EDII_PM_HANDLER_H
+#define PROJETO_01_EDII_PM_HANDLER_H
+
+#include "../include/exhash.h"
+
+void pm_processa_arquivo(const char *caminho_arquivo, exhash_t *mapa_habitantes);
+
+#endif //PROJETO_01_EDII_PM_HANDLER_H
---
+++ src/pm_handler.c
@@ -0,0 +1,85 @@
+#include "../include/pm_handler.h"
+#include "habitante.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "unity_memory.h"
+
+static void processa_comando_p(exhash_t *map, const char *linha_lida) {
+    // Declaramos os recipientes aqui dentro!
+    char cpf[16], nome[20], sobrenome[20], data_nascimento[12];
+    char sexo;
+
+    // O %*s no começo salva a sua vida: ele ignora o 'P'
+    sscanf(linha_lida, "%*s %15s %19s %19s %c %11s", cpf, nome, sobrenome, &sexo, data_nascimento);
+
+    // Cria o habitante (isso dá um malloc internamente)
+    habitante_t *novo = habitante_init(cpf, nome, sobrenome, sexo, data_nascimento);
+    if (novo == NULL) return;
+
+    // Tenta inserir no banco
+    if (!exhash_insert(map, novo, cpf)) {
+        printf("Habitante CPF %s nao inserido! (Ja existe no banco)\n", habitante_get_cpf(novo));
+    }
+
+    // Libera a RAM
+    habitante_destroy(novo);
+}
+
+static void processa_comando_m(exhash_t *mapa_habitantes, const char *linha_lida) {
+    assert(mapa_habitantes != NULL);
+
+    char cpf[16], cep[16], complemento[20] = "";
+    char face;
+    double num;
+
+    sscanf(linha_lida, "%*s %15s %15s %c %lf %19[^\n]", cpf, cep, &face, &num, complemento);
+
+    habitante_t *morador = (habitante_t *) exhash_remove(mapa_habitantes, cpf);
+
+    if (morador != NULL) {
+        habitante_set_endereco(morador, cep, face, num, complemento);
+
+        exhash_insert(mapa_habitantes, morador, cpf);
+
+        habitante_destroy(morador);
+
+
+        printf("Morador CPF %s mudou-se para o CEP %s!\n", cpf, cep);
+    }
+
+    else {
+        printf("Aviso: Comando 'm' falhou. CPF %s nao encontrado no banco.\n", cpf);
+    }
+}
+
+void pm_processa_arquivo(const char *caminho_arquivo, exhash_t *mapa_habitantes) {
+    assert(mapa_habitantes != NULL);
+
+    FILE *pm = fopen(caminho_arquivo, "r");
+    if (pm == NULL) {
+        printf("Aviso: Erro ao abrir o arquivo .pm\n");
+        return;
+    }
+
+    char linha[256];
+
+    while (fgets(linha, sizeof(linha), pm)) {
+        char comandos[3];
+
+        sscanf(linha,"%2s", comandos);
+
+        if (strcmp(comandos, "p") == 0) {
+            processa_comando_p(mapa_habitantes, linha);
+        }
+
+        else if (strcmp(comandos, "m") == 0) {
+            processa_comando_m(mapa_habitantes, linha);
+        }
+    }
+
+    fclose(pm);
+}
